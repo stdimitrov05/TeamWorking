0ebd245816840f600d302da00335760076d8e2f7
st_d05 <75424605+stdimitrov05@users.noreply.github.com>
2021-10-25 18:31:59 +0300

    add sour

---
+++ project/js/developerShow.js
@@ -0,0 +1,4 @@
+function developerShow(){
+    alert("След като натиснете ОК ще ви закара в github repostory и ще можете да видите ресурс кода на сайта. Последвате Симеон (Zumbata232) и Станислав (stdimitrov05)  ")
+    window.open("https://github.com/stdimitrov05/TeamWorking")
+}
