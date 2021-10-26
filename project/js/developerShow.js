80f64cca2471d21bb17066375b04c03ef6459171
st_d05 <75424605+stdimitrov05@users.noreply.github.com>
2021-10-26 12:12:15 +0300

    Index flex

--- project/js/developerShow.js
+++ project/js/developerShow.js
@@ -1,4 +1,27 @@
-function developerShow(){
-    alert("След като натиснете ОК ще ви закара в github repostory и ще можете да видите ресурс кода на сайта. Последвате Симеон (Zumbata232) и Станислав (stdimitrov05)  ")
-    window.open("https://github.com/stdimitrov05/TeamWorking")
+function developerShow() {
+  alert(
+    'След като натиснете ОК ще ви закара в github repostory и ще можете да видите ресурс кода на сайта. Последвате Симеон (Zumbata232) и Станислав (stdimitrov05)  ',
+  )
+  window.open('https://github.com/stdimitrov05/TeamWorking')
+
+  const newTest = (document.getElementById('devButton').innerHTML =
+    'Бутона беше кликнат!')
+
+  console.log(newTest)
+
+  document.querySelectorAll('.developer').forEach(function (e) {
+    e.addEventListener('click', function () {
+      this.style.backgroundColor = rgba(11, 250, 31, 0.801)
+    })
+  })
+  //_this.style.backgroundColor = "rgba(11, 250, 31, 0.801)"
+}
+
+function navigatorButton() {
+  var x = document.getElementById('myTopnav')
+  if (x.className === 'topnav') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'topnav'
+  }
 }
